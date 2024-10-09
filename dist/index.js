256c01dbb702f20778efe94ea2892dfeb94475f6
roee ltmanovich <roeeli770@gmail.com>
2024-10-09 10:24:53 +0300

    create basic server

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const PORT = (process.env.PORT || 1414);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.listen(PORT, () => console.log(`http://lochkost:${PORT}`));
