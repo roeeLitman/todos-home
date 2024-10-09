255b4a7ba366c66362138e559361510f07881909
roee ltmanovich <roeeli770@gmail.com>
2024-10-09 10:39:59 +0300

    created routs for user and todo, and connect them to index

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const usersRoute_1 = __importDefault(require("./routes/usersRoute"));
+const todosRoute_1 = __importDefault(require("./routes/todosRoute"));
 const PORT = (process.env.PORT || 1414);
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use('/user', usersRoute_1.default);
+app.use('/user', todosRoute_1.default);
 app.listen(PORT, () => console.log(`http://lochkost:${PORT}`));
